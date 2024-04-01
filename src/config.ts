43b1ed473627bbdb71327cdb360cea691bf321a8
Frédéric Kaczynski <frederic.kaczynski@sagacify.com>
2024-04-01 11:22:29 +0200

    Remove Datadog integration for log collection

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,6 @@ import "dotenv/config";
 
 export const LOG_LEVEL = process.env.LOG_LEVEL;
 export const NODE_ENV = process.env.NODE_ENV || "production";
-export const DATADOG_API_KEY = process.env.DATADOG_API_KEY || "no-key";
 export const UNSAFE_DATABASE_OPERATIONS =
     process.env.UNSAFE_DATABASE_OPERATIONS || false;
 export const DATABASE_HOSTNAME = process.env.DATABASE_HOSTNAME;
